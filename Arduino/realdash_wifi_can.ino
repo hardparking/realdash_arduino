727621293fe043f481a754c0c063bbee435b9071
Adam Wien <adam@cpanel.net>
2017-12-13 14:20:12 -0600

    moved wifi creds out of main code

--- Arduino/realdash_wifi_can.ino
+++ Arduino/realdash_wifi_can.ino
@@ -1,8 +1,11 @@
 #include <SPI.h>
 #include <WiFi101.h>
 
-char ssid[] = "";
-char pass[] = "";
+
+#include "arduino_secrets.h" 
+//please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP
 int status = WL_IDLE_STATUS;
 
 WiFiServer server(8000);
