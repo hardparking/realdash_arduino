2cb19d7469e79280f129208a068cb3401cebef1a
Adam Wien <adam@cpanel.net>
2017-12-21 12:52:39 -0600

    decode emu via arduino

---
+++ Arduino/decode_emu/decode_emu.ino
@@ -0,0 +1,38 @@
+#define EMU_FRAME_MAGIC 0xa3
+#pragma pack(push)
+#pragma pack(1)
+
+typedef struct _emu_frame {
+  uint8_t channel,
+          magic;
+  uint16_t value;
+  uint8_t checksum;
+} emu_frame;
+
+emu_frame frame;
+
+#pragma pack(pop)
+
+int count = 1;
+
+void setup() {
+      Serial.begin(19200);
+      Serial1.begin(19200);
+}
+
+void loop() {
+      size_t readlen;
+      if (Serial1.available() >= 5) {
+        while ((readlen = Serial1.readBytes((char *)&frame, sizeof(frame))) != 0) {
+          Serial.print("Channel: ");
+          Serial.println(frame.channel);
+          Serial.print("Magic: ");
+          Serial.println(frame.magic);
+          Serial.print("Value: ");
+          Serial.println(frame.value);
+          Serial.print("Checksum: ");
+          Serial.println(frame.checksum);
+        }
+      }
+}
+
