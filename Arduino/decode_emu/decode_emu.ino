3590435551e9fedc5fdb6f8f7da5198756c33ed1
Adam Wien <adam.wien@gmail.com>
2017-12-21 18:11:46 -0600

    added arrays for identifying channels, still needs testing

--- Arduino/decode_emu/decode_emu.ino
+++ Arduino/decode_emu/decode_emu.ino
@@ -1,3 +1,7 @@
+
+#       define be16toh(s) \
+            ((uint16_t)(((s & 0xff00) >> 8) | ((s & 0x00ff) << 8)))
+            
 #define EMU_FRAME_MAGIC 0xa3
 #pragma pack(push)
 #pragma pack(1)
@@ -12,10 +16,79 @@ typedef struct _emu_frame {
 emu_frame frame;
 
 #pragma pack(pop)
+const char *units[256] = {
+  "bunk",
+  "RPM",
+  "kPa",
+  "%",
+  "C",
+  "V",
+  "deg",
+  "ms",
+  "C",
+  "C",
+  "V",
+  "ms",
+  "AFR",
+  "gear",
+  "kPa",
+  "V",
+  "V",
+  "V",
+  "V",
+  "%",
+  "C",
+  "Bar",
+  "C",
+  "Bar",
+  "C",
+  "",
+  "C",
+  "λ",
+  "km/h",
+  "kPa",
+  "%",
+  "Table Set",
+  "λ",
+  "AFR", 
+};
 
 const char *channels[256] = {
+    "bunk",
     "RPM",
-    "MAP"
+    "MAP",
+    "TPS",
+    "IAT",
+    "Batt",
+    "IgnAngle",
+    "pulseWidth",
+    "Egt1",
+    "Egt2",
+    "knockLevel",
+    "dwellTime",
+    "wboAFR",
+    "gear",
+    "Baro",
+    "AnalogIn1",
+    "AnalogIn2",
+    "AnalogIn3",
+    "AnalogIn4",
+    "injDC",
+    "emuTemp",
+    "oilPressure",
+    "oilTemperature",
+    "fuelPressure",
+    "CLT",
+    "flexFuelEthanolContent",
+    "ffTemp",
+    "wboLambda",
+    "vssSpeed",
+    "deltaFPR",
+    "cel",
+    "fuelLevel",
+    "fuelLevel",
+    "lambdaTarget",
+    ""
 };
 
 void setup() {
@@ -35,14 +108,18 @@ void loop() {
               break;
             }
           }
-          Serial.print("Channel: ");
-          Serial.println(frame.channel);
-          Serial.print("Magic: ");
-          Serial.println(frame.magic, HEX);
-          Serial.print("Value: ");
-          Serial.println(frame.value);
-          Serial.print("Checksum: ");
-          Serial.println(frame.checksum);
+          if (frame.channel == 254) {
+            break;
+          }
+          if (channels[frame.channel] == "oilPressure") {
+          Serial.print(channels[frame.channel]);
+          Serial.print(": ");
+          Serial.print(be16toh(frame.value), DEC);
+          Serial.print(" ");
+          Serial.print(units[frame.channel]);
+          Serial.print("\n");
+          delay(1);
+          }
         }
       }
 }
