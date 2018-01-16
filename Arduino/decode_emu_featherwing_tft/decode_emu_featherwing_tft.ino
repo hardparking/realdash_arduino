b54cc6eaeb7b86b312fe751ead55468e7ed11197
Adam Wien <adam.wien@gmail.com>
2018-01-16 14:40:09 -0600

    stuff

--- Arduino/decode_emu_featherwing_tft/decode_emu_featherwing_tft.ino
+++ Arduino/decode_emu_featherwing_tft/decode_emu_featherwing_tft.ino
@@ -38,191 +38,86 @@ Adafruit_STMPE610 ts = Adafruit_STMPE610(STMPE_CS);
 
 int checksum = 0;
 
+struct {
+  const char *position;
+  int emu_channel;
+} frontPage[6] = {
+  { "1a", 1},
+  { "1b", 2},
+  { "2a", 3},
+  { "2b", 4},
+  { "3a", 5},
+  { "3b", 6}
+};
+
 uint16_t values[256];
 
-void render_null() {}
 
-void render_rpm(uint8_t channel) {
-  return be16toh(values[channel]);
+void render_rpm() {
+  tft.println(be16toh(values[1]));
 }
 
 void render_map(uint8_t channel) {
-  return be16toh(values[channel]);
+  tft.println(be16toh(values[channel]));
 }
 
 void render_tps(uint8_t channel) {
-  return be16toh(values[channel]);
+  tft.println(be16toh(values[channel]));
 }
 
 void render_iat(uint8_t channel) {
-  return be16toh(values[channel]);
+  tft.println(be16toh(values[channel]));
 }
 
 void render_bat(uint8_t channel) {
-  return be16toh(values[channel]) / 37;
+  tft.println(be16toh(values[channel]) / 37);
 }
 
 void render_ign(uint8_t channel) {
-  return be16toh(values[channel]) / 2;
-}
-
-void render_sinj(uint8_t channel) {
-  return be16toh(values[channel]) / 62;
-}
-
-void render_injpw(uint8_t channel) {
-  return be16toh(values[channel]) / 62;
-}
-
-void render_egt1(uint8_t channel) {
-  return be16toh(values[channel]);
-}
-
-void render_egt2(uint8_t channel) {
-  return be16toh(values[channel]);
-}
-
-void render_knklvl(uint8_t channel) {
-  return be16toh(values[channel]) / 51;
-}
-
-void render_dwell(uint8_t channel) {
-  return be16toh(values[channel]) / 20;
-}
-
-void render_afr(uint8_t channel) {
-  return be16toh(values[channel]) / 10;
-}
-
-void render_gear(uint8_t channel) {
-  return be16toh(values[channel]);
-}
-
-void render_baro(uint8_t channel) {
-  return be16toh(values[channel]);
-}
-
-void render_ang1(uint8_t channel) {
-  return be16toh(values[channel]) / 51;
-}
-
-void render_ang2(uint8_t channel) {
-  return be16toh(values[channel]) / 51;
-}
-
-void render_ang3(uint8_t channel) {
-  return be16toh(values[channel]) / 51;
-}
-
-void render_ang4(uint8_t channel) {
-  return be16toh(values[channel]) / 51;
-}
-
-void render_injdc(uint8_t channel) {
-  return be16toh(values[channel]) / 2;
-}
-
-void render_ecutmp(uint8_t channel) {
-  return be16toh(values[channel]);
-}
-
-void render_oilps(uint8_t channel) {
-  return be16toh(values[channel]) / 16;
-}
-
-void render_oiltmp(uint8_t channel) {
-  return be16toh(values[channel]);
-}
-
-void render_fuelps(uint8_t channel) {
-  return be16toh(values[channel]) / 32;
-}
-
-void render_clt(uint8_t channel) {
-  return be16toh(values[channel]);
-}
-
-void render_ffcon(uint8_t channel) {
-  return be16toh(values[channel]) / 2;
-}
-
-void render_fftmp(uint8_t channel) {
-  return be16toh(values[channel]);
-}
-
-void render_lambda(uint8_t channel) {
-  return be16toh(values[channel]) / 128;
-}
-
-void render_speed(uint8_t channel) {
-  return be16toh(values[channel]) / 4;
-}
-
-void render_fpdel(uint8_t channel) {
-  return be16toh(values[channel]);
-}
-
-void render_flvl(uint8_t channel) {
-  return be16toh(values[channel]);
-}
-
-void render_tbl(uint8_t channel) {
-  return be16toh(values[channel]);
-}
-
-void render_ltgt(uint8_t channel) {
-  return be16toh(values[channel]) / 100;
-}
-
-void render_afrtgt(uint8_t channel) {
-  return be16toh(values[channel]) / 10;
-}
-
-void render_cel(uint8_t channel) {
-  return be16toh(values[channel]);
+  tft.println(be16toh(values[channel]) / 2);
 }
 
 struct {
   const char *name, *unit;
   float divisor;
   int gaugeMin, gaugeMax;
-  void (*render)(uint8_t channel);
+  void (*render)();
 } channels[256] = {
-  { NULL, NULL, 0, 0, 0, render_null},
-  { "RPM", "RPM", 1, 0, 9000, render_rpm },
-  { "MAP", "kPa", 1, 0, 400, render_map },
-  { "TPS", "%", 1, 0, 100, render_tps },
-  { "IAT", "C", 1, -40, 120, render_iat },
-  { "Battery", "V", 37, 8, 20, render_bat },
-  { "Ign. Angle", "deg", 2, -20, 60, render_ign },
-  { "Sec. inj. PW", "ms", 62, 0, 25, render_sinj },
-  { "Inj. PW", "ms", 62, 0, 25, render_injpw },
-  { "EGT #1", "C", 1, 0, 1100, render_egt1 },
-  { "EGT #2", "C", 1, 0, 1100, render_egt2 },
-  { "Knock Level", "V", 51, 0, 5, render_knklvl },
-  { "Dwell Time", "ms", 20, 0, 10, render_dwell },
-  { "AFR", "AFR", 10, 10, 20, render_afr },
-  { "Gear", "", 1, 0, 6, render_gear },
-  { "BARO", "kPa", 1, 50, 120, render_baro },
-  { "Analog #1", "V", 51, 0, 5, render_ang1 },
-  { "Analog #2", "V", 51, 0, 5, render_ang2 },
-  { "Analog #3", "V", 51, 0, 5, render_ang3 },
-  { "Analog #4", "V", 51, 0, 5, render_ang4 },
-  { "Inj. DC", "%", 2, 0, 100, render_injdc },
-  { "ECU Temp.", "C", 1, -40, 120, render_ecutmp },
-  { "Oil press.", "Bar", 16, 0, 12, render_oilps },
-  { "Oil temp.", "C", 1, 0, 160, render_oiltmp },
-  { "Fuel press.", "Bar", 32, 0, 7, render_fuelps },
-  { "CLT", "C", 1, -40, 220, render_clt },
-  { "FF content", "%", 2, 0, 100, render_ffcon },
-  { "FF Temp", "C", 1, -30, 120, render_fftmp },
-  { "Lambda", "λ", 128, 0.7, 1.3, render_lambda },
-  { "Speed", "km/h", 4, 0, 300, render_speed },
-  { "FP delta", "kPa", 1, 100, 500, render_fpdel },
-  { "Fuel level", "%", 1, 0, 100, render_flvl },
-  { "Tables set", "", 1, 0, 1, render_tbl },
-  { "Lambda target", "λ", 100, 0.7, 1.3, render_ltgt },
-  { "AFR Target", "AFR", 10, 10, 20, render_afrtgt },
+  { NULL, NULL, 0, 0, 0},
+  { "RPM", "RPM", 1, 0, 9000, render_rpm  },
+  { "MAP", "kPa", 1, 0, 400 },
+  { "TPS", "%", 1, 0, 100 },
+  { "IAT", "C", 1, -40, 120 },
+  { "Battery", "V", 37, 8, 20 },
+  { "Ign. Angle", "deg", 2, -20, 60 },
+  { "Sec. inj. PW", "ms", 62, 0, 25 },
+  { "Inj. PW", "ms", 62, 0, 25 },
+  { "EGT #1", "C", 1, 0, 1100 },
+  { "EGT #2", "C", 1, 0, 1100 },
+  { "Knock Level", "V", 51, 0, 5 },
+  { "Dwell Time", "ms", 20, 0, 10 },
+  { "AFR", "AFR", 10, 10, 20 },
+  { "Gear", "", 1, 0, 6 },
+  { "BARO", "kPa", 1, 50, 120 },
+  { "Analog #1", "V", 51, 0, 5 },
+  { "Analog #2", "V", 51, 0, 5 },
+  { "Analog #3", "V", 51, 0, 5 },
+  { "Analog #4", "V", 51, 0, 5 },
+  { "Inj. DC", "%", 2, 0, 100 },
+  { "ECU Temp.", "C", 1, -40, 120 },
+  { "Oil press.", "Bar", 16, 0, 12 },
+  { "Oil temp.", "C", 1, 0, 160 },
+  { "Fuel press.", "Bar", 32, 0, 7 },
+  { "CLT", "C", 1, -40, 220 },
+  { "FF content", "%", 2, 0, 100 },
+  { "FF Temp", "C", 1, -30, 120 },
+  { "Lambda", "λ", 128, 0.7, 1.3 },
+  { "Speed", "km/h", 4, 0, 300 },
+  { "FP delta", "kPa", 1, 100, 500 },
+  { "Fuel level", "%", 1, 0, 100 },
+  { "Tables set", "", 1, 0, 1 },
+  { "Lambda target", "λ", 100, 0.7, 1.3 },
+  { "AFR Target", "AFR", 10, 10, 20 },
   { NULL, NULL, 0, 0, 0 },
   { NULL, NULL, 0, 0, 0 },
   { NULL, NULL, 0, 0, 0 },
@@ -443,14 +338,13 @@ struct {
   { NULL, NULL, 0, 0, 0 },
   { NULL, NULL, 0, 0, 0 },
   { NULL, NULL, 0, 0, 0 },
-  {"cel", "", 1, 0, 0, render_cel}
+  {"cel", "", 1, 0, 0}
 };
 
 
 void setup() {
   
   memset(&values, 0, sizeof(values));
-  pinMode(LED_BUILTIN, OUTPUT);
   
   tft.begin();
   if (!ts.begin()) {
@@ -479,13 +373,16 @@ void setup() {
   
   tft.drawRect(165, 165, 155, 75, ILI9341_WHITE); //3b
   //tft.fillRect(170, 170, 145, 65, ILI9341_RED);
-
+  
   
   
   Serial.begin(19200);
   Serial1.begin(19200);
+  tft.setCursor(20, 15);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
-  tft.setTextSize(4);
+  tft.setTextSize(5);
+  tft.setCursor(20, 15);
+
   
 }
 
@@ -496,26 +393,11 @@ void DEBUG() {
 
 int i;
 int newchannel;
-int count = 0;
-
-int menu1a = 1;
-int menu1b = 2;
-int menu2a = 3;
-int menu2b = 4;
-int menu3a = 5;
-int menu3b = 6;
-
-float old1a;
-float old1b;
-float old2a;
-float old2b;
-float old3a;
-float old3b;
-
-
+int count = 1;
+int d1a = 1;
 void loop() {
   
-  //TS_Point p = ts.getPoint();
+  TS_Point p = ts.getPoint();
   size_t readlen;
   emu_frame raw;
   /*
@@ -523,63 +405,61 @@ void loop() {
     while ((readlen = Serial1.readBytes((char *)&frame, sizeof(frame))) != 0) {
       uint8_t checksum;
       for (;;) {
-        checksum = frame.channel + frame.magic + ((frame.value & 0xff00) >> 8) + (frame.value & 0x00ff) % 254;
-        if (frame.magic != 0xa3 && frame.checksum != checksum) {
+        checksum = frame.channel + frame.magic + ((frame.value & 0xff00) >> 8) + (frame.value & 0x00ff) % 255;
+        if (checksum == frame.checksum) {
+          break;
+        }
+        if (frame.magic != 0xa3) {
           memmove(&frame, ((uint8_t *)&frame) + 1, sizeof(emu_frame) - 1);
           frame.checksum = (uint8_t)Serial1.read();
-          Serial.println("frame mismatch");
         } else {
-          values[frame.channel] = frame.value;
           break;
         }
       }
-      */
-
-      //p.x = map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
-      //p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
       
+      values[frame.channel] = frame.value;
+      */
+      p.x = map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
+      p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
       if (ts.touched()) {
         //tft.fillScreen(ILI9341_BLACK);
         TS_Point p = ts.getPoint();
         tft.setCursor(0, 0);
         if (p.x < 1200 && p.y < 1800) {
-          delay(100);
-          menu1a++;
-          //Serial.println("1a pushed");
+          Serial.println("1a pushed");
+          tft.fillRect(10, 10, 145, 65, ILI9341_RED);
         }
         if (p.x < 1200 && p.y > 2200) {
-          delay(100);
-          //frontPage[1].emu_channel = drawMenu(frontPage[1].emu_channel);
-          //Serial.println("1b pushed");
+          Serial.println("1b pushed");
         }
         if (p.x > 1500 && p.x < 2500 && p.y < 1900) {
-          delay(100);
-          //frontPage[2].emu_channel = drawMenu(frontPage[2].emu_channel);
           Serial.println("2a pushed");
         }
         if (p.x > 1500 && p.x < 2500 && p.y > 2000) {
-          delay(100);
-          //frontPage[3].emu_channel = drawMenu(frontPage[3].emu_channel);
           Serial.println("2b pushed");
         }
         if (p.x > 2800 && p.y < 1800) {
-          delay(100);
-          //frontPage[4].emu_channel = drawMenu(frontPage[4].emu_channel);
           Serial.println("3a pushed");
         }
         if (p.x > 2800 && p.y > 1800) {
-          delay(100);
-          //frontPage[5].emu_channel = drawMenu(frontPage[5].emu_channel);
           Serial.println("3b pushed");
         }
       }
 
-      //1a
+      values[1] = count;
+      count++;
       tft.setCursor(140, 8);
       tft.setTextSize(1);
-      tft.println(channels[menu1a].unit);
-
+      tft.println(channels[d1a].unit);
+      tft.setCursor(10, 62);
+      tft.setTextSize(2);
+      tft.println(channels[d1a].name);
+      tft.setCursor(20, 15);
+      tft.setTextSize(5);
+      channels[d1a].render();
+      //render_rpm();
       /*
+      
       //1a
       tft.drawRect(5, 5, 155, 75, ILI9341_WHITE);
       tft.setCursor(140, 8);
@@ -665,17 +545,6 @@ void loop() {
       tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
       tft.println(values[frontPage[5].emu_channel], 0);
       */
-//    }
-//  }
+    //}
+  //}
 }
-
-int drawMenu(int curchannel) {
-  if (curchannel >= 35) {
-    curchannel = 1;
-  } else {
-    curchannel++;
-  }
-  return curchannel;
-}
-
-
